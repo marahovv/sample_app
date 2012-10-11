3a430054a3a122209c36257df0e0da1ad0e39348
root <root@marahovv-X38-DS4.(none)>
2012-10-12 00:41:02 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '62a7540ec0d6a525d32e515cfcaeeb21491ae54be1fb83338cc5187f7a51aa1d1b5d70c772aca42bd43bc0afc2ea8d6f43d64342267da42172c561a8b0a155a5'
